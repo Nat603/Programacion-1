2b546b17fce595f582205bdcf4a2aebb5476a77a
Nat603 <natalia.garciao@uqvirtual.edu.co>
2024-02-24 20:23:33 -0500

    Pruebas de veterinaria corriendo

--- universidad/src/main/veterinaria1/src/test/java/co/edu/uniquindio/poo/MascotaTest.java
+++ universidad/src/main/veterinaria1/src/test/java/co/edu/uniquindio/poo/MascotaTest.java
@@ -8,6 +8,8 @@
 package co.edu.uniquindio.poo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 
@@ -39,5 +41,40 @@ public class MascotaTest {
         LOG.info("Finalizando test datos completos");
     }
 
-    
+
+    @Test
+    public void datosNulos() {
+        LOG.info("Iniciado test datos nulos");
+
+
+        assertThrows(Throwable.class, ()-> new Mascota (null,null,null, (byte) 0, null,null, (byte) 3));
+
+
+        LOG.info("Finalizando test datos nulos");
+    }
+
+
+    @Test
+    public void edadNegativa() {
+        LOG.info("Iniciado test edad negativa");
+
+        assertThrows(Throwable.class, ()-> new Mascota ("Flufy", "Pomerania", "Cara de oso", (byte) -2, "Femenino", "negro", (byte) 3));
+
+        LOG.info("Finalizando test edad negativa");
+    }
+
+
+    @Test
+    public void datosVacios() {
+        LOG.info("Iniciado test datos vacios");
+
+        assertThrows(Throwable.class, ()-> new Mascota ("Flufy","Pomerania","", (byte) 2, "Femenino","", (byte) 3));
+
+        LOG.info("Finalizando test datos vacios");
+    }
 }
+
+
+
+
+
