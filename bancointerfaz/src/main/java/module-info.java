8b9305a1ac6fece9e00214d5c8b57f35fa39f18a
Nat603 <natalia.garciao@uqvirtual.edu.co>
2024-05-08 12:03:34 -0500

    quite la interfaz sin el model

--- bancointerfaz/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.poo {
-    requires transitive javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-    opens co.edu.uniquindio.poo to javafx.fxml, controller;
-    exports co.edu.uniquindio.poo;
-    
-}
